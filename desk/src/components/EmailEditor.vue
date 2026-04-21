861a25adcfc2df2a005b96d3ad0b53fef9334ae3
Ravi Kant Kumar <kancomsystems@gmail.com>
2026-04-21 17:03:37 +0530

    fix: reply boundary and comm-area flex layout (probe verified)

--- desk/src/components/EmailEditor.vue
+++ desk/src/components/EmailEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <TextEditor
-    class="flex flex-col max-h-full"
+    class="flex flex-col flex-1 min-h-0 max-h-full"
     ref="editorRef"
     :editor-class="[
       'prose-sm max-w-full mx-6 md:mx-10 py-3',
